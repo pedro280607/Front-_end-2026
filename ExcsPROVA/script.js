e9c64c43410d54704b9cc2788c0ee3721451342d
pedro280607 <pedrohppaluno@gmail.com>
2026-04-23 09:37:08 -0300

    Add files via upload

---
+++ ExcsPROVA/script.js
@@ -0,0 +1,31 @@
+function proximoGrupo() {
+    document.getElementById("grupo-a").innerHTML = `
+        <h2>🅳 Grupo D</h2>
+        <ul>
+            <li>Estados Unidos</li>
+            <li>Paraguai</li>
+            <li>Austrália</li>
+            <li>Turquia</li>
+        </ul>
+    `;
+
+    document.getElementById("grupo-b").innerHTML = `
+        <h2>🅴 Grupo E</h2>
+        <ul>
+            <li>Alemanha</li>
+            <li>Equador</li>
+            <li>Costa do Marfim</li>
+            <li>Curaçao</li>
+        </ul>
+    `;
+
+    document.getElementById("grupo-c").innerHTML = `
+        <h2>🅵 Grupo F</h2>
+        <ul>
+            <li>Holanda</li>
+            <li>Japão</li>
+            <li>Tunísia</li>
+            <li>Suécia</li>
+        </ul>
+    `;
+}
